9890720175ec614c7a3b036208bd39ddfcb05237
Killsudo <cody@killsudo.info>
2013-09-12 02:44:15 -0500

    How to use dicts and modify them on the fly.

---
+++ dict_list_mods.py
@@ -0,0 +1,18 @@
+inventory = {'gold' : 500,
+'pouch' : ['flint', 'twine', 'gemstone'], # Assigned a new list to 'pouch' key
+'backpack' : ['xylophone','dagger', 'bedroll','bread loaf']}
+
+# Adding a key 'burlap bag' and assigning a list to it
+inventory['burlap bag'] = ['apple', 'small ruby', 'three-toed sloth']
+inventory['pocket'] = ['seashell', 'strange berry', 'lint']
+
+# Sorting the list found under the key 'pouch'
+inventory['pouch'].sort()
+inventory['backpack'].sort()
+# Here the dictionary access expression takes the place of a list name 
+
+inventory['backpack'].remove('dagger')
+
+inventory['gold'] = inventory['gold'] + 50
+
+print inventory['gold']
